4c952fec1cee2c4eb8db0ac9b6b5b8697fc91e2a
飞刀 <makediff@qq.com>
2024-01-13 15:15:55 +0800

    up

--- src/bgconsts.ts
+++ src/bgconsts.ts
@@ -3,8 +3,8 @@ export class BGCONSTS {
     static SERVICE_ID: number = 34
     static MARKET_INFO: string = 'siyuan-plugin'
     static ENDPOINT: string = 'siyuanwucaiplugin'
-    static VERSION: string = '24.1.13'
-    static VERSION_NUM: number = 240113
+    static VERSION: string = '24.1.14'
+    static VERSION_NUM: number = 240114
     static IS_DEBUG: boolean = false
     static BASE_URL: string = 'https://marker.dotalk.cn'
     static CLIENT_ID_KEY: string = 'wc-SiYuanClientId'
